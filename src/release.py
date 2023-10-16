94b368240115234fc9ee1b3b60be07fe190b3445
yonekuramiki <mikiyonekura4040@gmail.com>
2023-10-16 23:40:54 +0900

    Change release

--- src/release.py
+++ src/release.py
@@ -5,8 +5,12 @@ clone_dir = '/Users/yonekuramiki/Desktop/resarch/searchSATD-underCode/clone_rele
 
 # リポジトリをクローンする
 #repo = git.Repo.clone_from('https://github.com/mikiyonekura/chervil-hp.git', clone_dir)
-repo = git.Repo.clone_from('https://github.com/argouml-tigris-org/argouml.git', clone_dir)
+repo = git.Repo.clone_from('https://github.com/apache/ant.git', clone_dir)
+
 
 tags = repo.tags
 for tag in tags:
     print(tag.name)
+
+#一時的に追加
+repo.git.checkout('rel/1.7.0')
